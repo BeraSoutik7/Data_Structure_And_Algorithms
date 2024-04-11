9a05b3078ac34b14dd7f70cfad858a606129c6fc
SudipaGuchhait <guchhaitsudipa005@gmail.com>
2024-04-11 14:22:26 +0530

    test

---
+++ src/Tree/BinaryTree/BinaryTree.java
@@ -0,0 +1,32 @@
+package Tree.BinaryTree;
+
+ 
+
+     class Node {
+         int data;
+         Node left;
+         Node right;
+
+        public Node (int data) {
+             this.data = data;
+             this.left = null;
+             this.right = null;
+         }
+     }
+
+     public  class BinaryTree {
+          static int idx = -1;
+          public static Node buildTree(int nodes []){
+              idx++;
+             if (nodes[idx] == -1) {
+                 return null;
+             }
+             Node newNode = new Node(nodes[idx]);
+             newNode.left = buildTree(nodes);
+             newNode.right = buildTree(nodes);
+
+             return newNode;
+         }
+     }
+
+
---
+++ src/Tree/BinaryTree/Example1.java
@@ -0,0 +1,17 @@
+package Tree.BinaryTree;
+
+public class Example1 {
+    public static void main(String[] args){
+
+        BinaryTree  tree  = new BinaryTree();
+        tree.nodes (8);
+        tree.insert(7);
+        treeinsert(12);
+        tree.insert(15);
+        bt.insert(2);
+        bt.insert(5);
+
+
+
+    }
+}
